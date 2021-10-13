931286e8aea1b2397034ef400bf660fe25a8e103
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2021-10-13 15:27:56 +1100

    done

--- src/BestCodrEver/TransferCheck/CommandLoader.php
+++ src/BestCodrEver/TransferCheck/CommandLoader.php
@@ -4,12 +4,14 @@ namespace BestCodrEver\TransferCheck;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use BestCodrEver\TransferCheck\TransferCommand;
 
 class CommandLoader extends PluginBase {
 
     public function onEnable(){
         $commandMap = Server::getInstance()->getCommandMap();
         $commandMap->unregister($commandMap->getCommand('transferserver'));
+        $commandMap->registerCommands($plugin->getName(), new TransferCommand($plugin));
     }
     
 }
